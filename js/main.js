033cd4821d14d9ad149ddae13f128b5a8e54e73d
luigilalo <luigilaloe1@gmail.com>
2023-11-13 15:07:33 +0100

    Some adjustements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-let km = parseInt(prompt("Quanti km vuoi perccorrere?"));
+let km = parseInt(prompt("Quanti km vuoi percorrere?"));
 if (isNaN(km)) {
     alert("Non hai inserito un numero!");
 }
@@ -12,11 +12,15 @@ let PriceForKm = 0.21;
 
 let TotalPrice;
 
+
 if (age > 18 && age < 65) {TotalPrice = km * PriceForKm;}
 
 else if (age < 18) {TotalPrice = (km * PriceForKm) - (km * PriceForKm) * 20 / 100 }
 
 else if (age > 65) {TotalPrice = (km * PriceForKm) - (km * PriceForKm) * 40 / 100 }
+
+Number(TotalPrice).toFixed(2)
+
 
 // Compilazione ConsoleLog
 let TicketInformation = `
